a6336c1f6ce0a9503debeded66bcb654e57c2e90
furuCRM-NganTo <128334694+furuCRM-NganTo@users.noreply.github.com>
2023-06-07 17:37:12 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 export NVM_DIR=$HOME/.nvm;
 . $NVM_DIR/nvm.sh;
 
-cd /home/ec2-user/deploy_nextjs_app
+cd /home/ec2-user/nextjs
 git pull origin master
 nvm install v18.16.0
 npm install &&
 npm run build &&
-pm2 restart deploy_nextjs_app
+pm2 restart nextjs
